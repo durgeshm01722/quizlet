33c5df009104dca598ad527dbd1b544191adf20e
durgeshm01722 <durgeshmahajan1722@gmail.com>
2023-02-23 20:55:39 +0530

    Fixed navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -17,20 +17,22 @@ export default function Navbar() {
   }, [])
 
   return (
-    <div className='bg-[#00243b] h-[70px] w-[100%] flex-row items-center fixed backdrop-blur-md bg-opacity-60 z-10'>
-      <div className='flex flex-row items-center ml-16'>
-        <Image src={require('../../assets/logo.png')} width={40} height={40} className='mr-4' alt="logo"></Image>
-        <p className='text-[30px] font-semibold text-gray-300'>Quizlet</p>
-      </div>
-      <div className='mx-[20%]'>
-        <ul className='flex flex-row justify-center items-center text-gray-300 gap-20'>
-          <Link href='/home'>Home</Link>
-          <Link href='/about'>About Quizlet</Link>
-          <Link href='/contactus'>Contact Us</Link>
-        </ul>
-      </div>
-      <div>
-        {!user.loggedin ? <button className='primary-btn mr-4' onClick={() => router.push("/login")}>Login</button> : <button className='secondary-btn' onClick={() => { alert("Logged out successfully!!"); user.toggleLoggedin(); localStorage.clear(); router.push("/home"); }}>Logout</button>}
+    <div className='bg-[#00243b] h-[70px] w-[100%] flex-row justify-start items-center fixed backdrop-blur-md bg-opacity-60 z-10'>
+      <div className='flex flex-row justify-start items-center h-fit w-[100%] gap-[3%]'>
+        <div className='flex flex-row items-center ml-16'>
+          <Image src={require('../../assets/logo.png')} width={40} height={40} className='mr-4' alt="logo"></Image>
+          <p className='text-[30px] font-semibold text-gray-300'>Quizlet</p>
+        </div>
+        <div className='mx-[20%]'>
+          <ul className='flex flex-row justify-center items-center text-gray-300 gap-20'>
+            <Link href='/home'>Home</Link>
+            <Link href='/about'>About Quizlet</Link>
+            <Link href='/contactus'>Contact Us</Link>
+          </ul>
+        </div>
+        <div className='w-fit'>
+          {!user.loggedin ? <button className='primary-btn mr-4' onClick={() => router.push("/login")}>Login</button> : <button className='secondary-btn' onClick={() => { alert("Logged out successfully!!"); user.toggleLoggedin(); localStorage.clear(); router.push("/home"); }}>Logout</button>}
+        </div>
       </div>
     </div>
   )
