097b80ffd7617e17640fb92faac00c8c5d4e4867
durgeshm01722 <durgeshmahajan1722@gmail.com>
2023-02-27 01:28:04 +0530

    Added pagination, Server Side Rendering, Match the following questions

--- src/pages/quiz.js
+++ src/pages/quiz.js
@@ -5,24 +5,27 @@ import { useRouter } from 'next/router';
 import axios from 'axios';
 
 var answers = [];
+const chunk = 10;
+var length = [];
 
-export default function quiz() {
+export default function quiz(props) {
     const [name, setName] = useState("");
     const [questions, setQuestions] = useState([]);
+    const [page, setPage] = useState(0);
+    const [paginatedQuestions, setPaginatedQuestions] = useState([]);
     const [stats, setStats] = useState({ correct: 0, incorrect: 0 });
 
     const router = useRouter();
 
-    const { quantity, type, difficulty } = router.query;
+    const { quantity, type, difficulty } = props.query;
 
-    const getQuestions = async () => {
-        const res = await axios.get(`https://opentdb.com/api.php?amount=${quantity}&category=${type}&difficulty=${difficulty.toLowerCase()}&type=multiple`);
-        setQuestions(res.data.results);
-        answers = [];
-    }
 
     const handleClick = (e, key) => {
         answers[key] = e.target.value;
+    }
+
+    const changePage = (page) => {
+        setPage(page);
     }
 
     const calculate = () => {
@@ -38,10 +41,27 @@ export default function quiz() {
 
     useEffect(() => {
         setName(localStorage.getItem("name"));
-        if (router.isReady) {
-            getQuestions();
+        setQuestions(props.data.results);
+        answers = [];
+        if (length.length !== quantity / 10) {
+            for (let i = 1; i <= (quantity / 10); i++) {
+                length.push(i);
+            }
         }
-    }, [router.isReady]);
+    }, []);
+
+    useEffect(() => {
+        if (questions[0]) {
+            if (router.query.quantity > 10) {
+                for (let i = 0; i < (quantity / 10); i++) {
+                    setPaginatedQuestions((prev) => [...prev, questions.slice(chunk * i, chunk * (i + 1))]);
+                }
+            }
+            else {
+                setPaginatedQuestions([questions]);
+            }
+        }
+    }, [questions]);
 
     return (
         <div className='h-[1800px] bg-[#021622] flex-c-center'>
@@ -62,21 +82,24 @@ export default function quiz() {
                     <div className='h-[70%] flex flex-col border-[1px] border-gray-700 w-[70%] mt-4 rounded overflow-y-scroll p-8'>
                         <div>
                             {
-                                questions.map((item, key) => {
+                                paginatedQuestions[0] && paginatedQuestions[page].map((item, key) => {
                                     return (
                                         <div key={key} className='flex flex-col justify-start items-start border-[1px] border-gray-800 h-[300px] w-[100%] my-8 rounded p-[3%]'>
-                                            <p className='text-white mb-6'>{item.question}</p>
+                                            <div className='flex-r-center'>
+                                                <p className='text-white mb-6 mr-4'>{(key + 1) + ((page) * 10)}.</p>
+                                                <p className='text-white mb-6'>{item.question}</p>
+                                            </div>
                                             <label htmlFor={`options${key}`} className="text-white">
-                                                <input type="radio" value={item.incorrect_answers[0]} name={`options${key}`} className='m-4' onChange={(e) => handleClick(e, key)} />{item.incorrect_answers[0]}
+                                                <input type="radio" value={item.incorrect_answers[0]} name={`options${key}`} className='m-4' onChange={(e) => handleClick(e, ((key + 1) + ((page) * 10)) - 1)} />{item.incorrect_answers[0]}
                                             </label>
                                             <label htmlFor={`options${key}`} className="text-white">
-                                                <input type="radio" value={item.incorrect_answers[0]} name={`options${key}`} className='m-4' onChange={(e) => handleClick(e, key)} />{item.incorrect_answers[1]}
+                                                <input type="radio" value={item.incorrect_answers[0]} name={`options${key}`} className='m-4' onChange={(e) => handleClick(e, ((key + 1) + ((page) * 10)) - 1)} />{item.incorrect_answers[1]}
                                             </label>
                                             <label htmlFor={`options${key}`} className="text-white">
-                                                <input type="radio" value={item.correct_answer} name={`options${key}`} className='m-4' onChange={(e) => handleClick(e, key)} />{item.correct_answer}
+                                                <input type="radio" value={item.correct_answer} name={`options${key}`} className='m-4' onChange={(e) => handleClick(e, ((key + 1) + ((page) * 10)) - 1)} />{item.correct_answer}
                                             </label>
                                             <label htmlFor={`options${key}`} className="text-white">
-                                                <input type="radio" value={item.incorrect_answers[0]} name={`options${key}`} className='m-4' onChange={(e) => handleClick(e, key)} />{item.incorrect_answers[2]}
+                                                <input type="radio" value={item.incorrect_answers[0]} name={`options${key}`} className='m-4' onChange={(e) => handleClick(e, ((key + 1) + ((page) * 10)) - 1)} />{item.incorrect_answers[2]}
                                             </label>
                                         </div>
                                     )
@@ -84,10 +107,26 @@ export default function quiz() {
                             }
                         </div>
                     </div>
-
+                    <div className='flex flex-row justify-center items-center w-fit mt-8'>
+                        {length.map((i, key) => {
+                            return (<button key={key} className='w-[60px] h-[50px] bg-[#1e2e4d] text-white rounded mx-2' onClick={() => {
+                                changePage(i - 1);
+                            }}>{i}</button>)
+                        })}
+                    </div>
                     <button className='green-btn w-[150px] mt-8' onClick={() => calculate()}>Submit Quiz</button>
                 </>
             }
         </div>
     )
+}
+
+
+export async function getServerSideProps(context) {
+    const { quantity, type, difficulty } = context.query;
+    const res = await fetch(`https://opentdb.com/api.php?amount=${quantity}&category=${type}&difficulty=${difficulty.toLowerCase()}&type=multiple`)
+    const data = await res.json();
+    return {
+        props: { data, query: context.query }
+    }
 }
